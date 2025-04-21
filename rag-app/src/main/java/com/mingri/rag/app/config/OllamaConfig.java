37afae1100933655078f9124cff09663e98cb08b
mingri31164 <3116430062@qq.com>
2025-04-21 23:55:58 +0800

    feat：扩展OpenAI模型对接

--- rag-app/src/main/java/com/mingri/rag/app/config/OllamaConfig.java
+++ rag-app/src/main/java/com/mingri/rag/app/config/OllamaConfig.java
@@ -4,6 +4,8 @@ import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.ai.ollama.OllamaEmbeddingClient;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.openai.OpenAiEmbeddingClient;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.PgVectorStore;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
@@ -25,6 +27,11 @@ public class OllamaConfig {
         return new OllamaChatClient(ollamaApi);
     }
 
+    @Bean
+    public OpenAiApi openAiApi(@Value("${spring.ai.openai.base-url}") String baseUrl, @Value("${spring.ai.openai.api-key}") String apikey) {
+        return new OpenAiApi(baseUrl, apikey);
+    }
+
 
     /**
      * 向量库切割文件
@@ -38,21 +45,31 @@ public class OllamaConfig {
      * 简化向量库
      **/
     @Bean
-    public SimpleVectorStore vectorStore(OllamaApi ollamaApi) {
+    public SimpleVectorStore vectorStore(@Value("${spring.ai.rag.embed}") String model, OllamaApi ollamaApi, OpenAiApi openAiApi) {
+        if ("nomic-embed-text".equalsIgnoreCase(model)) {
             OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
             embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
             return new SimpleVectorStore(embeddingClient);
+        } else {
+            OpenAiEmbeddingClient embeddingClient = new OpenAiEmbeddingClient(openAiApi);
+            return new SimpleVectorStore(embeddingClient);
         }
+    }
 
 
     /**
      * 向量数据库存储
      **/
     @Bean
-    public PgVectorStore pgVectorStore(OllamaApi ollamaApi, JdbcTemplate jdbcTemplate) {
+    public PgVectorStore pgVectorStore(@Value("${spring.ai.rag.embed}") String model, OllamaApi ollamaApi, OpenAiApi openAiApi, JdbcTemplate jdbcTemplate) {
+        if ("nomic-embed-text".equalsIgnoreCase(model)) {
             OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
             embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
             return new PgVectorStore(jdbcTemplate, embeddingClient);
+        } else {
+            OpenAiEmbeddingClient embeddingClient = new OpenAiEmbeddingClient(openAiApi);
+            return new PgVectorStore(jdbcTemplate, embeddingClient);
         }
+    }
 
 }
--- rag-trigger/src/main/java/com/mingri/rag/trigger/http/OpenAiController.java
+++ rag-trigger/src/main/java/com/mingri/rag/trigger/http/OpenAiController.java
@@ -8,8 +8,8 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.ollama.OllamaChatClient;
-import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.vectorstore.PgVectorStore;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.web.bind.annotation.*;
@@ -23,31 +23,42 @@ import java.util.stream.Collectors;
 
 @RestController()
 @CrossOrigin("*")
-@RequestMapping("/api/v1/ollama/")
-public class OllamaController implements IAiService {
+@RequestMapping("/api/v1/openai/")
+public class OpenAiController implements IAiService {
 
     @Resource
-    private OllamaChatClient chatClient;
+    private OpenAiChatClient chatClient;
     @Resource
     private PgVectorStore pgVectorStore;
 
     @RequestMapping(value = "generate", method = RequestMethod.GET)
     @Override
-    public ChatResponse generate(@RequestParam String model, @RequestParam String message) {
-        return chatClient.call(new Prompt(message, OllamaOptions.create().withModel(model)));
+    public ChatResponse generate(@RequestParam("model") String model, @RequestParam("message") String message) {
+        return chatClient.call(new Prompt(
+                message,
+                OpenAiChatOptions.builder()
+                        .withModel(model)
+                        .build()
+        ));
     }
 
 
     @RequestMapping(value = "generate_stream", method = RequestMethod.GET)
     @Override
-    public Flux<ChatResponse> generateStream(@RequestParam String model, @RequestParam String message) {
-        return chatClient.stream(new Prompt(message, OllamaOptions.create().withModel(model)));
+    public Flux<ChatResponse> generateStream(@RequestParam("model") String model, @RequestParam("message") String message) {
+        return chatClient.stream(new Prompt(
+                message,
+                OpenAiChatOptions.builder()
+                        .withModel(model)
+                        .build()
+        ));
     }
 
 
     @RequestMapping(value = "generate_stream_rag", method = RequestMethod.GET)
     @Override
     public Flux<ChatResponse> generateStreamRag(@RequestParam("model") String model, @RequestParam("ragTag") String ragTag, @RequestParam("message") String message) {
+
         String SYSTEM_PROMPT = """
                 Use the information from the DOCUMENTS section to provide accurate answers but act as if you knew this information innately.
                 If unsure, simply state that you don't know.
@@ -71,8 +82,9 @@ public class OllamaController implements IAiService {
 
         return chatClient.stream(new Prompt(
                 messages,
-                OllamaOptions.create()
+                OpenAiChatOptions.builder()
                         .withModel(model)
+                        .build()
         ));
     }
 
